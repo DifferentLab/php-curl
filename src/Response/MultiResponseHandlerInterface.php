3d0b9dc70064cd2b0ada67ba84b80a6bc1d5dedc
smiley <smiley@chillerlan.net>
2016-02-16 23:04:39 +0100

    nah, thats just stupid.

--- src/Response/MultiResponseHandlerInterface.php
+++ src/Response/MultiResponseHandlerInterface.php
@@ -1,8 +1,10 @@
 <?php
 /**
+ * Interface MultiResponseHandlerInterface
  *
  * @filesource   MultiResponseHandlerInterface.php
  * @created      16.02.2016
+ * @package      chillerlan\TinyCurl\Response
  * @author       Smiley <smiley@chillerlan.net>
  * @copyright    2016 Smiley
  * @license      MIT
@@ -10,19 +12,10 @@
 
 namespace chillerlan\TinyCurl\Response;
 
-use chillerlan\TinyCurl\MultiRequest;
-
 /**
- * @property \chillerlan\TinyCurl\MultiRequest request
+ *
  */
 interface MultiResponseHandlerInterface{
-
-	/**
-	 * MultiResponseHandlerTest constructor.
-	 *
-	 * @param \chillerlan\TinyCurl\MultiRequest $request
-	 */
-	public function __construct(MultiRequest &$request);
 
 	/**
 	 * @param \chillerlan\TinyCurl\Response\ResponseInterface $response
